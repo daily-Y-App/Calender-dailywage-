8df8e558acfdcdede1555429f2cfce6932dadff8
YZ <ya292755@gmail.com>
2026-02-08 23:51:54 +0700

    Refactor service worker for better readability

--- sw.js
+++ sw.js
@@ -2,14 +2,18 @@ const CACHE_NAME = 'work-calc-v2';
 const ASSETS = [
   './',
   './index.html',
+  './manifest.json',
   'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js'
 ];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
 });
-
